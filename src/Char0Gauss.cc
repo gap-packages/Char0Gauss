58d8bc954f8a364cbb608182d9cf1b0a17f78d85
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-03-10 17:27:06 +0000

    Naive implementation of SemiEchelonMat for rational matrices
    This runs slower than the generic version.

--- src/Char0Gauss.cc
+++ src/Char0Gauss.cc
@@ -10,20 +10,132 @@
 
 extern "C" {
 #include "src/compiled.h"          /* GAP headers                */
+    #include "src/rational.h"
+
+    Int EqRat(Obj l, Obj r);
+    Obj ZeroRat(Obj x);
+    Obj AInvRat(Obj x);
+    Obj InvRat(Obj x);
+    Obj ProdRat(Obj l, Obj r);
+    Obj SumRat(Obj l, Obj r);
+    Obj DiffRat(Obj l, Obj r); // Hack, because AInvRat does not accept integers..
 }
 
+/*
+ * Fairly naive (but hopefully faster) gaussian elimination for
+ * matrices of rationals
+ * Input is assumed to be a list of lists of correct shape, not empty, containing only rationals,
+ * and all the rest breakage of it
+ *
+ * This function will destroy its input and if you care about it save
+ * it ahead of time!
+ */
 
-Obj TestCommand(Obj self)
+// dst += src * a
+static inline Obj AddRationalRows(Obj dst, Obj src, Obj a)
 {
-    return INTOBJ_INT(42);
+    Obj tmp;
+    UInt i;
+
+    for(i=1; i <= LEN_PLIST(dst); i++) {
+        tmp = ELM_PLIST(src, i);
+        tmp = ProdRat(tmp, a);
+        tmp = SumRat(ELM_PLIST(dst,i),tmp);
+        SET_ELM_PLIST(dst, i, tmp);
+        CHANGED_BAG(dst);
+    }
+    return 0L;
 }
 
-Obj TestCommandWithParams(Obj self, Obj param, Obj param2)
+static inline Obj MultRationalRow(Obj dst, Obj a)
 {
-    /* simply return the first parameter */
-    return param;
+    Obj tmp;
+    UInt i;
+
+    for(i=1;i<=LEN_PLIST(dst);i++) {
+        tmp = ProdRat(ELM_PLIST(dst, i), a);
+        SET_ELM_PLIST(dst, i, tmp);
+        CHANGED_BAG(dst);
+    }
+    return 0L;
 }
 
+Obj SemiEchelonRationals(Obj self, Obj mat)
+{
+    Int p;
+    UInt i, j;
+    UInt nrows, ncols;
+    UInt nvecs, nheads, nnzheads;
+
+    Obj row;
+    Obj vectors;
+    Obj heads;
+    Obj result;
+    Obj nzheads;
+    Obj x;
+    Obj zero;
+    Obj inv;
+    Obj tmp, tmp2;
+
+    // ZeroRat ignores its argument...
+    zero = ZeroRat(inv);
+
+    nrows = LEN_PLIST(mat);
+    ncols = LEN_PLIST(ELM_PLIST(mat, 1));
+
+    heads = NEW_PLIST(T_PLIST, ncols);
+    nheads = 1;
+    SET_LEN_PLIST(heads, 0);
+
+    vectors = NEW_PLIST(T_PLIST, ncols);
+    nvecs = 1;
+    SET_LEN_PLIST(vectors, 0);
+
+    nzheads = NEW_PLIST(T_PLIST, ncols);
+    nnzheads = 1;
+    SET_LEN_PLIST(nzheads, 0);
+
+    for(i=1;i<=nrows;i++) {
+        row = ELM_PLIST(mat, i);
+
+        for(j=1;j<nnzheads;j++) {
+            p = INT_INTOBJ(ELM_PLIST(nzheads, j));
+
+            x = ELM_PLIST(row, p);
+            if (EqRat(x, zero) == 0) {
+                tmp = ELM_PLIST(vectors, j);
+                tmp2 = DiffRat(zero, x);
+                AddRationalRows(row, tmp, tmp2);
+            }
+        }
+
+        /* First non-zero position */
+        for(j=1;EqRat(ELM_PLIST(row,j), zero);j++) {
+        };
+        if(j<=ncols){
+            /* New basis vector */
+
+            /* This should, can't and will not fail. It will NOT FAIL */
+            inv = InvRat(ELM_PLIST(row,j));
+            MultRationalRow(row, inv);
+            SET_LEN_PLIST(vectors, nvecs);
+            SET_ELM_PLIST(vectors, nvecs++, row);
+            CHANGED_BAG(vectors);
+            SET_LEN_PLIST(nzheads, nnzheads);
+            SET_ELM_PLIST(nzheads, nnzheads++, INTOBJ_INT(j));
+            CHANGED_BAG(nzheads);
+            SET_LEN_PLIST(heads, j);
+            SET_ELM_PLIST(heads, j, INTOBJ_INT(nvecs-1));
+            CHANGED_BAG(heads);
+        }
+    }
+
+    result = NEW_PREC(2);
+    AssPRec(result, RNamName("heads"), heads);
+    AssPRec(result, RNamName("vectors"), vectors);
+
+    return result;
+}
 
 typedef Obj (* GVarFunc)(/*arguments*/);
 
@@ -35,8 +147,7 @@ typedef Obj (* GVarFunc)(/*arguments*/);
 
 // Table of functions to export
 static StructGVarFunc GVarFuncs [] = {
-    GVAR_FUNC_TABLE_ENTRY("Char0Gauss.c", TestCommand, 0, ""),
-    GVAR_FUNC_TABLE_ENTRY("Char0Gauss.c", TestCommandWithParams, 2, "param, param2"),
+    GVAR_FUNC_TABLE_ENTRY("Char0Gauss.c", SemiEchelonRationals, 1, "mat"),
 
 	{ 0 } /* Finish with an empty entry */
 
